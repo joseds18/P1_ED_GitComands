8edbe8ffc31129c88eba2c5c16005c4df7643b87
joseds18 <159795914+joseds18@users.noreply.github.com>
2025-05-05 11:09:53 +0200

    Update calculadora.java

--- Ejercicio4_1/src/ejercicio4_1/calculadora.java
+++ Ejercicio4_1/src/ejercicio4_1/calculadora.java
@@ -11,9 +11,5 @@ public class calculadora {
      public int multiplicar(int a, int b){
          return a * b;
      }
-
-      public double potencia(double base, double exponente) {
-        return Math.pow(base, exponente);
-    }
       
 }
